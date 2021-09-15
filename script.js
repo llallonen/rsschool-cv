148a93c3f6c9dc08b2bf2b5065908f4b049f2e8f
llallonen <llallonen@gmail.com>
2021-09-15 18:59:13 +0300

    feat: wrote a script for a burger menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const menu = document.querySelector('.menu');
+const burger = document.querySelector('.burger-menu');
+const photo = document.querySelector('.photo');
 
 menu.addEventListener('click', (event) => {
   let target = event.target;
@@ -18,10 +20,29 @@ menu.addEventListener('click', (event) => {
   let wantedSection = document.getElementById(attr.slice(1));
   console.log(wantedSection);
   
-
+  openCloseBurger();
   const y = wantedSection.getBoundingClientRect().top + window.scrollY;
     window.scroll({
     top: y,
     behavior: 'smooth'
   });
+})
+
+burger.onclick = openCloseBurger;
+
+function openCloseBurger() {
+  burger.classList.toggle("burger-menu--open");
+  document.querySelector('.menu').classList.toggle('menu--active');
+
+  if (window.matchMedia("(min-width: 560px)" && "(max-width: 768px)").matches) {
+    photo.classList.toggle('photo--translated');
+  }
+}
+
+window.addEventListener('resize', () => {
+  if (window.matchMedia("(min-width: 900px)").matches) { 
+    if (burger.classList.contains('burger-menu--open')) {
+      openCloseBurger();
+    }
+  }
 })
